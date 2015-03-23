2e203be1c53f5fa18daeb1625b659d7d18983f75
Fehervari Sandor <h952649@stud.u-szeged.hu>
2015-03-24 00:29:33 +0100

    added skew detection and filtering only the relevant part of the image

--- ImgProc/main.cpp
+++ ImgProc/main.cpp
@@ -15,12 +15,16 @@ extern "C" {
 
 using namespace std;
 
+cv::Mat findBiggestBlob(cv::Mat & matImage);
+
 int main(){
 
-    Mat box = imread("img/pfm20det.png",1);
+    Mat box = imread("img/pfm20det.png");
     Mat gray;
     Mat detected_edges, blurred;
     cvtColor(box, gray, COLOR_RGB2GRAY);
+    
+
     
     Ptr<MSER> ms  = MSER::create(9,100,1000);
 
@@ -35,12 +39,29 @@ int main(){
     
     imshow("binary",binary);
     
+    
+    Mat hsvImage, hsvTresh;
+    cvtColor(box, hsvImage, COLOR_BGR2HSV);
+    imshow("hsvimage", hsvImage);
+    inRange(hsvImage,Scalar(18,100,120),Scalar(30,255,230),hsvTresh);
+    imshow("hsvtresh", hsvTresh);
+    
+    Mat biggestYellowBlob = findBiggestBlob(hsvTresh);
+    
+    imshow("biggestyellow", biggestYellowBlob);
+    
+    Mat filtered;
+    binary.copyTo(filtered, biggestYellowBlob);
+    imshow("filtered", filtered);
+    filtered.copyTo(binary);
     blur( gray, blurred, Size(3,3) );
     
     Canny( gray, detected_edges, 20, 20*3 );
     
     Mat result;
     binary.copyTo(result, detected_edges);
+    
+    
     
     vector<vector<Point> > contours;
     vector<Vec4i> hierarchy;
@@ -68,25 +89,35 @@ int main(){
     {
         approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
         boundRect[i] = boundingRect( Mat(contours_poly[i]) );
+        
+        minEnclosingCircle( Mat(contours_poly[i]),
+                           center[i], radius[i] );
+        
+        RotatedRect rect = minAreaRect(Mat(contours_poly[i]));
+        cout << rect.angle <<endl;
+        
+        Point2f vertices[4];
+        rect.points(vertices);
+        for (int i = 0; i < 4; i++)
+            line(box, vertices[i], vertices[(i+1)%4], Scalar(0,255,0));
+        
+        Mat res = box(boundRect[i]);
+        
+        
     }
     
 
-    
-    
-    
-    
     /// Draw polygonal contour + bonding rects
     Mat drawing = Mat::zeros( binary.size(), CV_8UC3 );
     char s[3] = "ss";
+    
+    int deltax = 0;
     for( int i = 0; i< contours.size(); i++ )
     {
         Scalar color = Scalar(255,0,255);
         drawContours( drawing, contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
         rectangle( box, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
-        Mat res = result(boundRect[i]);
         
-        sprintf(s, "%d", i);
-        imshow(s, result(boundRect[i]));
     }
     Size newSize(binary.size().width/2, binary.size().height/2);
     /*    for (int i = 0; i < regions.size(); i++)
@@ -115,4 +146,36 @@ int main(){
     return 0;
 
 
+}
+
+
+Mat findBiggestBlob(cv::Mat &src){
+    int largest_area=0;
+    int largest_contour_index=0;
+    Mat temp(src.rows,src.cols,CV_8UC1);
+    Mat dst(src.rows,src.cols,CV_8UC1,Scalar::all(0));
+    Rect boundRect;
+    src.copyTo(temp);
+    
+    vector<vector<Point>> contours; // storing contour
+    vector<Vec4i> hierarchy;
+    
+    findContours( temp, contours, hierarchy,RETR_CCOMP, CHAIN_APPROX_SIMPLE );
+    
+    for( int i = 0; i< contours.size(); i++ ) // iterate
+    {
+        double a=contourArea( contours[i],false);  //Find the largest area of contour
+        if(a>largest_area)
+        {
+            largest_area=a;
+            largest_contour_index=i;
+        }
+        
+    }
+
+    boundRect = boundingRect(Mat(contours[largest_contour_index]));
+    drawContours( dst, contours,largest_contour_index, Scalar(255), FILLED, 8, hierarchy );
+    rectangle( dst, boundRect.tl(), boundRect.br(), Scalar(255), FILLED, 8, 0 );
+    // Draw the largest contour
+    return dst;
 }
